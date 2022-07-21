e415d243ce4939a3144fe0dcfc88139f37b58c08
craciu25yt <48807342+craciu25yt@users.noreply.github.com>
2022-07-21 12:45:03 +1000

    Add files via upload

---
+++ v1.2.bat
@@ -0,0 +1,46 @@
+@echo off
+IF not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /B)
+title Last Activity View deleter ^| By AuchenDevs
+
+
+:main
+echo.
+echo.
+echo [1]. Delete
+echo.
+echo [2]. Exit
+echo.
+echo.
+set /p opc=Option: 
+if "%opc%"=="1" goto delete
+if "%opc%"=="1" del %~f0
+
+
+echo No valid option selected!
+timeout 2 /nobreak >nul
+goto main
+
+:delete
+
+
+::Logon... reg
+for /F  %%a in ('wevtutil.exe el') do (wevtutil.exe cl "%%a" && set totalev+=1)
+
+::Delete "View Folder in Explorer"
+reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" /f
+
+::Delete MUICache (Thats for muicache viewer but anyways)
+reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache" /f
+
+::xd 
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU" /f
+
+
+::Delete recents
+takeown /f "%appdata%\Microsoft\Windows\Recent" /r /d s
+for /f %%a in ('dir "%appdata%\Microsoft\Windows\Recent\*" /b /s') do (del /f /s /q "%%a" && set /a totalr+=1)
+
+::Delete prefetch the last one for hide all
+takeown /f C:\Windows\Prefetch /r /d s
+for /f %%a in ('dir C:\Windows\Prefetch /b') do (del /f /s /q C:\Windows\Prefetch\%%a && set /a totalp+=1)
+goto main
